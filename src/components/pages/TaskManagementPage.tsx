e7863b39545f746ff2a7657b1b84ffa0f0f4ef84
marksplendex <122271672+marksplendex@users.noreply.github.com>
2023-05-10 15:34:43 +0200

    feat: Admin can do the following actions to tasks: edit, delete, add user, remove user

--- src/components/pages/TaskManagementPage.tsx
+++ src/components/pages/TaskManagementPage.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@emotion/react";
 import {
   Button,
+  ListItemIcon,
   Menu,
   MenuItem,
   TableCell,
@@ -8,11 +9,13 @@ import {
   Tooltip,
   createTheme,
 } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import React, { useCallback, useEffect, useState } from "react";
 import { Navbar } from "../UI/Navbar";
 import { DefaultTable } from "../UI/DefaultTable";
 import axios from "axios";
 import { EditTaskPanel } from "../UI/EditTaskPanel";
+import { Logout } from "@mui/icons-material";
 
 export const TaskManagementPage = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -27,8 +30,12 @@ export const TaskManagementPage = () => {
   const [originalTaskStatus, setOriginalTaskStatus] = useState("");
   const [originalTaskUserId, setOriginalTaskUserId] = useState("");
   const [originalTaskProjectId, setOriginalTaskProjectId] = useState("");
+  const [deletingTask, setDeletingTask] = useState(false);
   const [edit, setEdit] = useState(false);
-
+  const [addingUserToTask, setAddingUserToTask] = useState(false);
+  const [removingUserFromTask, setRemovingUserFromTask] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [users, setUsers] = useState([]);
   const open = Boolean(anchorEl);
   const theme = createTheme({
     palette: {
@@ -51,14 +58,23 @@ export const TaskManagementPage = () => {
             },
           })
           .then((res) => {
+            res.data.forEach((task: any) => {
+              if (task.user === null) {
+                task.user = { name: "No user" };
+              }
+              if (task.project === null) {
+                task.project = { name: "No project" };
+              }
+            });
             setTableData(res.data);
+            console.log(res.data);
           });
       } catch (err) {
         console.log(err);
       }
     };
     fetchTasks();
-  }, []);
+  }, [removingUserFromTask, addingUserToTask]);
 
   const updateTask = useCallback(async () => {
     try {
@@ -69,8 +85,6 @@ export const TaskManagementPage = () => {
             name: taskName || originalTaskName || "",
             description: taskDescription || originalTaskDescription || "",
             status: taskStatus || originalTaskStatus || "",
-            user: originalTaskUserId || "",
-            project: originalTaskProjectId || "",
           },
           {
             headers: {
@@ -79,7 +93,16 @@ export const TaskManagementPage = () => {
           }
         )
         .then((res) => {
+          res.data.forEach((task: any) => {
+            if (task.user === null) {
+              task.user = { name: "No user" };
+            }
+            if (task.project === null) {
+              task.project = { name: "No project" };
+            }
+          });
           setTableData(res.data);
+          console.log(res.data);
         });
     } catch (err) {
       console.log(err);
@@ -88,11 +111,103 @@ export const TaskManagementPage = () => {
     setEdit(false);
   }, [edit]);
 
+  const deleteTask = useCallback(async () => {
+    try {
+      await axios
+        .delete(`http://localhost:3000/task/${taskId}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        })
+        .then((res) => {
+          res.data.forEach((task: any) => {
+            if (task.user === null) {
+              task.user = { name: "No user" };
+            }
+            if (task.project === null) {
+              task.project = { name: "No project" };
+            }
+          });
+          setTableData(res.data);
+          console.log(res.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+    setDeletingTask(false);
+  }, [deletingTask]);
+
+  const addUserToTask = useCallback(async () => {
+    try {
+      await axios.post(
+        `http://localhost:3000/user/${taskId}/task`,
+        { id: userId },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+    setAddingUserToTask(false);
+  }, [userId]);
+
+  const removeUserFromTask = useCallback(async () => {
+    try {
+      console.log(taskId);
+      await axios.delete(`http://localhost:3000/user/${taskId}/task`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    setRemovingUserFromTask(false);
+  }, [removingUserFromTask]);
+
+  const getAllUsers = useCallback(async () => {
+    try {
+      await axios
+        .get("http://localhost:3000/user/", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        })
+        .then((res) => {
+          setUsers(res.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   useEffect(() => {
+    getAllUsers();
     if (edit) {
       updateTask();
     }
-  }, [edit]);
+    if (deletingTask) {
+      deleteTask();
+    }
+    if (addingUserToTask) {
+      addUserToTask();
+    }
+    if (removingUserFromTask) {
+      removeUserFromTask();
+    }
+    if (addingUserToTask) {
+      addUserToTask();
+    }
+  }, [
+    edit,
+    deletingTask,
+    addingUserToTask,
+    removingUserFromTask,
+    addingUserToTask,
+  ]);
 
   const handleStartEdit = (task: any) => {
     setTaskId(task.id);
@@ -105,6 +220,32 @@ export const TaskManagementPage = () => {
   };
   const handleEdit = () => {
     setEdit(true);
+    console.log(taskName, taskDescription, taskStatus);
+  };
+
+  const handleDelete = (taskId: string) => {
+    setDeletingTask(true);
+    setTaskId(taskId);
+  };
+
+  const handleAddingUserToTask = (userId: string, taskId: string) => {
+    setTaskId(taskId);
+    setUserId(userId);
+    setAddingUserToTask(true);
+    setAnchorEl(null);
+    console.log(userId, taskId);
+  };
+  const handleRemoveUserFromTask = (taskId: string) => {
+    setRemovingUserFromTask(true);
+    setTaskId(taskId);
+  };
+
+  const handleClickOnDropDown = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
@@ -141,11 +282,58 @@ export const TaskManagementPage = () => {
                 {task.archived ? "Archived" : "Unarchived "}
               </TableCell>
               <TableCell>
+                {task.user.name === "No user" && (
+                  <div>
+                    <Tooltip title="Add User">
+                      <Button
+                        id={task.id}
+                        variant="contained"
+                        onClick={(e) => handleClickOnDropDown(e)}
+                      >
+                        Add User
+                      </Button>
+                    </Tooltip>
+                    <Menu
+                      anchorEl={anchorEl}
+                      id="users"
+                      open={open}
+                      onClose={handleClose}
+                    >
+                      {users.map((user: any) => (
+                        <MenuItem
+                          key={user.id}
+                          onClick={() =>
+                            handleAddingUserToTask(user.id, task.id)
+                          }
+                        >
+                          {user.name}
+                        </MenuItem>
+                      ))}
+                    </Menu>
+                  </div>
+                )}
+                {task.user.name !== "No user" && (
+                  <Button
+                    onClick={() => handleRemoveUserFromTask(task.id)}
+                    variant="contained"
+                  >
+                    Remove User
+                  </Button>
+                )}
+
                 <Button
                   variant="contained"
                   onClick={() => handleStartEdit(task)}
                 >
                   Edit
+                </Button>
+                <Button
+                  color="error"
+                  variant="outlined"
+                  id={task.id}
+                  onClick={() => handleDelete(task.id)}
+                >
+                  <DeleteIcon />
                 </Button>
               </TableCell>
             </TableRow>
